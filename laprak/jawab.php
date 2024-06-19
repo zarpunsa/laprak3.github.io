9f8e84dbaf2846e1712f62e1c002fa9c0fb01705
Muhammad Rizky Bamby Saputra <166977618+zarpunsa@users.noreply.github.com>
2024-06-19 21:20:30 +0700

    Add files via upload

---
+++ laprak/jawab.php
@@ -0,0 +1,6 @@
+<?php
+echo "<center>Nama Lengkap: ".$_POST['nama']."</center><br>";
+echo "<center>Email: ".$_POST['email']."</center><br>";
+echo "<center>Divisi: ".$_POST['div']."</center><br>";
+echo "<center>Alasan: ".$_POST['alasan']."</center><br>";
+?>
